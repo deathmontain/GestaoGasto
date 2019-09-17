18a4b94ca4bb1612e12d70460d6aebd305495f9b
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-17 08:58:46 -0300

    Servico de autenticacao.

--- src/main/java/br/com/jonatas/gestaoGasto/config/security/AutenticacaoService.java
+++ src/main/java/br/com/jonatas/gestaoGasto/config/security/AutenticacaoService.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 
 @Service
