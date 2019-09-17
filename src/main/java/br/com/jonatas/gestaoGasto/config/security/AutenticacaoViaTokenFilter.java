f007a70a071dec98cf3649dd4022fb2d19172737
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-17 08:57:21 -0300

    Autenticacao via token.

--- src/main/java/br/com/jonatas/gestaoGasto/config/security/AutenticacaoViaTokenFilter.java
+++ src/main/java/br/com/jonatas/gestaoGasto/config/security/AutenticacaoViaTokenFilter.java
@@ -7,7 +7,6 @@ import br.com.jonatas.gestaoGasto.repository.ClienteRepository;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
