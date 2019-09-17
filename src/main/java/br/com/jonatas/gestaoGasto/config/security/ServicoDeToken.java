77b4902c8aec04ca5e56d46a0e409e0f24f9ad68
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-17 08:49:02 -0300

    Criado o servico de token.

--- src/main/java/br/com/jonatas/gestaoGasto/config/security/ServicoDeToken.java
+++ src/main/java/br/com/jonatas/gestaoGasto/config/security/ServicoDeToken.java
@@ -8,7 +8,6 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
-
 import java.util.Date;
 
 @Service
