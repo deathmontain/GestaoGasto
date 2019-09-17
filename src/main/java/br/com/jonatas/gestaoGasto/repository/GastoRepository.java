440e5fa8bef8074cde41d99c4c8a0e319ffeb955
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-17 08:39:27 -0300

    Criado o o repository de gasto.

--- src/main/java/br/com/jonatas/gestaoGasto/repository/GastoRepository.java
+++ src/main/java/br/com/jonatas/gestaoGasto/repository/GastoRepository.java
@@ -5,7 +5,6 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.solr.repository.SolrCrudRepository;
-
 import java.util.List;
 import java.util.Optional;
 
