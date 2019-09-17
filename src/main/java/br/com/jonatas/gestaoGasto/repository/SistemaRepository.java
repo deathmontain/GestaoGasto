3d33af4cff7a9ed27892e662de923bd61b96f9a0
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-17 08:40:06 -0300

    Criado o repository sistema.

--- src/main/java/br/com/jonatas/gestaoGasto/repository/SistemaRepository.java
+++ src/main/java/br/com/jonatas/gestaoGasto/repository/SistemaRepository.java
@@ -3,7 +3,6 @@ package br.com.jonatas.gestaoGasto.repository;
 import br.com.jonatas.gestaoGasto.modelo.Sistema;
 import org.springframework.data.solr.repository.Query;
 import org.springframework.data.solr.repository.SolrCrudRepository;
-
 import java.util.Optional;
 
 public interface SistemaRepository extends SolrCrudRepository<Sistema, String> {
