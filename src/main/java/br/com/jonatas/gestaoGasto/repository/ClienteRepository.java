70b4de38537944c3589b7853236aa25254a53380
Jônatas Santos <jonatas.santos@aedu.com>
2019-09-17 08:30:52 -0300

    Criado o padrao repositori do modelo cliente.

--- src/main/java/br/com/jonatas/gestaoGasto/repository/ClienteRepository.java
+++ src/main/java/br/com/jonatas/gestaoGasto/repository/ClienteRepository.java
@@ -2,7 +2,6 @@ package br.com.jonatas.gestaoGasto.repository;
 
 import br.com.jonatas.gestaoGasto.modelo.Cliente;
 import org.springframework.data.solr.repository.SolrCrudRepository;
-
 import java.util.Optional;
 
 
